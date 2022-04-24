092d508bec6470b0a617250ec40573cc56281c6c
MugureNjunge <maureennjunge22@gmail.com>
2022-04-24 15:03:48 +0300

    create variables in the user class

--- passkeep_test.py
+++ passkeep_test.py
@@ -1,5 +1,13 @@
 class user :
 
+  users = ['Maureen', 'Evans']
+  caps = string.ascii_uppercase
+  numbers = []
+  for i in range(0,10):
+    numbers.append(str(i))
+  special_chars = ['@', '#', '!']  
+  name_check = False
+
   def __init__(self, username, password ):
 
     self.username = username
