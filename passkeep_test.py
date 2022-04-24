20b44d576402e2e50caf35124e0df67accafdd1e
MugureNjunge <maureennjunge22@gmail.com>
2022-04-24 15:11:28 +0300

    creating username and password conditions

--- passkeep_test.py
+++ passkeep_test.py
@@ -8,12 +8,45 @@ class user :
   special_chars = ['@', '#', '!']  
   name_check = False
 
-  def __init__(self, username, password ):
+  # ensuring username starts with caps
+  def add_name(name):
+    if name.title() in users :
+      return False
+    else:
+      return True
 
-    self.username = username
-    self.password = password
+      # ensuring username contains no numbers
+  def number_added(name):
+    found = False
+    for number in numbers:
+      if name.find(number)> -1:
+        found =True
+    return found  
 
-class credentials(user):
-  def __init__ (self, instagram, twitter):
-    self.instagram = instagram
-    self.twitter = twitter
+    # ensure password has uppercase
+  def caps_added(pw):
+    found = False
+    for letter in caps :
+      if pw.find(letter) >-1:
+        found = True
+    return found  
+
+    #  ensure password has special characters 
+  def special_added(pw):
+    found = False
+    for char in special_chars:
+      if pw.find(char) >-1:
+        found = True
+    return found  
+ 
+
+
+
+
+
+    
+
+# class credentials(user):
+#   def __init__ (self, instagram, twitter):
+#     self.instagram = instagram
+#     self.twitter = twitter
