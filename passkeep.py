0ab2f57be6803be8565c5c8533593456d354a036
MugureNjunge <maureennjunge22@gmail.com>
2022-04-24 14:43:29 +0300

    create test file and main file

---
+++ passkeep.py
@@ -0,0 +1,17 @@
+class user :
+
+  def __init__(self, first, last, username):
+     
+    import sys
+
+name = sys.argv[1]
+
+first = str(input())
+last = str(input())
+username = str(input())
+
+print(first)  
+print(last)  
+print(username)
+
+
---
+++ passkeep_test.py
@@ -0,0 +1,7 @@
+# Check against other usernames
+# password of 8 characters
+
+# class user 
+#   def__init__(Self, Maureen, Evans):
+
+
