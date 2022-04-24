c18fb24f858fbb14dc628f5627f37d02b4a39f3a
MugureNjunge <maureennjunge22@gmail.com>
2022-04-24 17:18:49 +0300

    adding username and password validation

--- classes.py
+++ classes.py
@@ -1,18 +1,14 @@
-# import string
-# from accounts import add_name
+users = ['Maureen', 'Evans']
+caps = string.ascii_uppercase
+numbers = []
+for i in range(0,10):
+  numbers.append(str(i))
+special_chars = ['@', '#', '!']  
+name_check = False
 
 
-# users = ['Maureen', 'Evans']
-# caps = string.ascii_uppercase
-# numbers = []
-# for i in range(0,10):
-#   numbers.append(str(i))
-# special_chars = ['@', '#', '!']  
-# name_check = False
+class user :
 
-
-# class user :
-
-#   classmethod@
-  # def __ init __ ()
+  classmethod@
+  def __ init __ ()
 
